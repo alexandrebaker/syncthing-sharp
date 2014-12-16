530e63fef0b85c5c523842ee147758fb723c0b37
Alexandre Baker <alexandre.baker@gmail.com>
2014-12-16 11:26:52 -0500

    clean some message header tests

--- Tests/Syncthing.Tests.Protocol/MessageHeaderTest.cs
+++ Tests/Syncthing.Tests.Protocol/MessageHeaderTest.cs
@@ -6,7 +6,7 @@ using Syncthing.Protocol.Messages;
 namespace Syncthing.Tests.Protocol
 {
     [TestClass]
-    public class MessagesTest
+    public class MessageHeaderTest
     {
         [TestMethod]
         public void TestHeaderFunctions()
@@ -40,7 +40,7 @@ namespace Syncthing.Tests.Protocol
         public void TestHeaderLayout4Bits()
         {
             // Version are the first four bits
-            uint e = 0xf0000000;
+            const uint e = 0xf0000000;
             uint a = Header.Encode(new Header {Version = 0xf});
             Assert.AreEqual(e, a, String.Format("Header layout incorrect; {0:X} != {1:X}", a, e));
         }
@@ -49,7 +49,7 @@ namespace Syncthing.Tests.Protocol
         public void TestHeaderLayout8Bits()
         {
             // Version are the first four bits
-            uint e = 0x0000ff00;
+            const uint e = 0x0000ff00;
             uint a = Header.Encode(new Header { MessageType = 0xff });
             Assert.AreEqual(e, a, String.Format("Header layout incorrect; {0:X} != {1:X}", a, e));
         }
@@ -58,7 +58,7 @@ namespace Syncthing.Tests.Protocol
         public void TestHeaderLayout12Bits()
         {
             // Version are the first four bits
-            uint e = 0x0fff0000;
+            const uint e = 0x0fff0000;
             uint a = Header.Encode(new Header { MessageId = 0xfff });
             Assert.AreEqual(e, a, String.Format("Header layout incorrect; {0:X} != {1:X}", a, e));
         }
